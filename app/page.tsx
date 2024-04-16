678aa911f53e7d6618826f0516070966d88008a6
GEREGUR <georgerubaylo@gmail.com>
2024-04-16 13:37:24 +0300

    finished UI for NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return ( <h1 className="text-3xl text-center">Typl init</h1> );
+  return <h1 className="text-3xl text-center bg-red-200">Typl init</h1>;
 }

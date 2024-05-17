d8965ef9bd24d8d9ca8b36fdda9d5afbd432bd0a
George Rubailo <georgerubailo@Georges-MacBook-Pro.local>
2024-05-17 04:56:20 +0300

    a

--- app/api/userExists/route.ts
+++ app/api/userExists/route.ts
@@ -1,8 +1,8 @@
 import User from "@/app/models/user";
 import { connectMongoDB } from "@/lib/mongodb";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function POST(req: NextResponse) {
+export async function POST(req: NextRequest) {
   try {
     await connectMongoDB();
     const { login } = await req.json();
@@ -11,5 +11,9 @@ export async function POST(req: NextResponse) {
     return NextResponse.json({ user });
   } catch (error) {
     console.log(error);
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
   }
 }

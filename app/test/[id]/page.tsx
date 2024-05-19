89b8df329ec6d1d6296e30289ffe090e3bf0fdd2
George Rubailo <georgerubailo@Georges-MacBook-Pro.local>
2024-05-20 00:12:31 +0300

    finished frontend

--- app/test/[id]/page.tsx
+++ app/test/[id]/page.tsx
@@ -1,22 +1,34 @@
-// app/test/[id]/page.tsx
+import { Button } from "@/components/ui/button";
 import { notFound } from "next/navigation";
 
 const mockData = {
   101: {
     name: "Block 1",
-    description: "Lorem ipsum ada love react with all my heart",
+    title: "Формирование культурно-бытовых ценностей",
+    description: "Организованный ли вы человек?",
+    questions: [{ question: "Is this a test question?", answer: "да" }],
+    color: "bg-[#FED7AA]",
   },
   102: {
     name: "Block 2",
-    description: "Lorem ipsum ada love react with all my heart",
+    title: "Формирование культурно-бытовых ценностей",
+    description: "Организованный ли вы человек?",
+    questions: [{ question: "Is this a test question?", answer: "нет" }],
+    color: "bg-[#FECDD3]",
   },
   103: {
     name: "Block 3",
-    description: "Lorem ipsum ada love react with all my heart",
+    title: "Формирование культурно-бытовых ценностей",
+    description: "Организованный ли вы человек?",
+    questions: [{ question: "Is this a test question?", answer: "да" }],
+    color: "bg-[#BFDBFE]",
   },
   104: {
     name: "Block 4",
-    description: "Lorem ipsum ada love react with all my heart",
+    title: "Формирование культурно-бытовых ценностей",
+    description: "Организованный ли вы человек?",
+    questions: [{ question: "Is this a test question?", answer: "нет" }],
+    color: "bg-[#BBF7D0]",
   },
 };
 
@@ -32,9 +44,35 @@ export default function TestPage({ params }: { params: { id: string } }) {
   }
 
   return (
-    <div>
-      <h1>{block.name}</h1>
-      <p>{block.description}</p>
+    <div
+      className={`${block.color} min-h-screen w-full flex flex-col items-center justify-start text-white p-48 text-center gap-12`}
+    >
+      <p className="text-2xl text-white">{block.title}</p>
+      <h1 className="text-5xl tracking-tight font-bold">{block.description}</h1>
+      <Button
+        className="bg-black/15 shadow-md w-48 text-2xl hover:text-black/15 hover:shadow-xl duration-300 hover:border-2 hovere:border-black/25"
+        variant={"ghost"}
+      >
+        Пройти
+      </Button>
+      <p className="pb-24 font-semibold text-2xl">
+        Пройдите тест и узнайте, насколько хорошо вы организованы в быту, умеете
+        управлять финансами и ресурсами, а также заботиться о своем окружении.
+        Этот тест поможет вам оценить вашу способность эффективно управлять
+        своими делами и ресурсами, а также выявить области, в которых вы можете
+        улучшить свои навыки хозяйственности.
+      </p>
+      {/* <div>
+        <h2>Questions:</h2>
+        <ul>
+          {block.questions.map((q: any, index: any) => (
+            <li key={index}>
+              <p>{q.question}</p>
+              <p>Answer: {q.answer}</p>
+            </li>
+          ))}
+        </ul>
+      </div> */}
     </div>
   );
 }
